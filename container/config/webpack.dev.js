04019bc607b654920e297281bf272cf9ccb3ca32
Saurabh Singh Bhadauria - PO <Saurabh.Bhadauria@coforge.com>
2025-01-14 12:16:23 +0530

    change git ignore and config files

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
@@ -17,9 +16,6 @@ const devConfig = {
       name: "container",
       remotes: { marketing: "marketing@http://localhost:8081/remoteEntry.js" },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
